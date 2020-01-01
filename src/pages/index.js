820786927bbb8d9eb9e6d5b7afe935f48ceba2c6
Bill Garrett <garrett@peppermile.com>
2020-01-01 09:35:38 -0800

    Sort front page cards by icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,22 +4,51 @@ import { graphql, Link } from 'gatsby'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const cardName = edge => {
-  const frontName = edge.node.context.card.front.name
-  const backName = edge.node.context.card.back.name
+import { append, groupBy, keys, map, pluck, pathOr, reduce } from 'ramda'
+
+const cardName = node => {
+  const frontName = node.context.card.front.name
+  const backName = node.context.card.back.name
   return (frontName === backName) ? frontName : `${frontName} / ${backName}`
 }
+
+const groupedCards = data => {
+  const cardGroups = groupBy(pathOr('Core Cards', ['context', 'card', 'front', 'icons']), pluck('node', data.allSitePage.edges))
+  return reduce(
+    (a, icons) => append({
+      icons,
+      nodes: cardGroups[icons]
+    }, a),
+    [],
+    keys(cardGroups)
+  )
+}
+
+/*
+    <ul>
+    {data.allSitePage.edges.map((edge) => (
+      
+    ))}
+    </ul>
+
+ */
 
 const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
-    <ul>
-    {data.allSitePage.edges.map((edge) => (
-      <li>
-        <Link to={edge.node.path}>{cardName(edge)}</Link>
-      </li>
-    ))}
-    </ul>
+    {map((cardGroup) => (
+      <div>
+        <h3>{cardGroup.icons}</h3>
+        <ul>
+          {map(node => (
+            <li>
+              <Link to={node.path}>{cardName(node)}</Link>
+            </li>
+          ), cardGroup.nodes)}
+        </ul>
+        <pre>{JSON.stringify(cardGroup.cards)}</pre>
+      </div>
+    ), groupedCards(data))}
   </Layout>
 )
 
@@ -35,9 +64,11 @@ query AllCardPages {
           card {
             front {
               name
+              icons
             }
             back {
               name
+              icons
             }
           }
         }
