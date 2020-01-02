db1ff8a6929c6b4dd5ebc576bfaafe35b9617f7c
Bill Garrett <garrett@peppermile.com>
2020-01-01 21:40:45 -0800

    Place card fronts and backs side by side

--- src/components/card.js
+++ src/components/card.js
@@ -1,20 +1,30 @@
 import React from "react"
 import { map } from 'ramda'
 
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import CardFace from "./card-face"
 
-const CardPage = ({pageContext: {title, cards}}) => (
+const CardPage = ({ pageContext: { title, cards } }) => (
   <Layout>
     <SEO title={title} />
-    {map(card => (
-      <div>
-        <CardFace side={card.front} brand="Front" />
-        <CardFace side={card.back} brand="Back" />
-      </div>
-    ), cards)}
+    <Container>
+      {map(card => (
+        <Row>
+          <Col sm={6}>
+            <CardFace side={card.front} brand="Front" />
+          </Col>
+          <Col sm={6}>
+            <CardFace side={card.back} brand="Back" />
+          </Col>
+        </Row>
+      ), cards)}
+    </Container>
   </Layout>
 )
 
