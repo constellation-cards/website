a4178569f56660c746ad028c292a9d2b2dad406f
Bill Garrett <garrett@peppermile.com>
2020-01-01 19:08:22 -0800

    Use multiple badges for tags

--- src/components/card-face.js
+++ src/components/card-face.js
@@ -2,14 +2,22 @@ import React from "react"
 import Badge from "react-bootstrap/Badge"
 import Card from "react-bootstrap/Card"
 
-import { join } from 'ramda'
+import { /* join, */ map } from 'ramda'
+
+// const singleBadge = tags => <Badge variant="secondary">{join(', ', tags || [])}</Badge>
+
+const multiBadge = tags => map(tag => (
+  <span>
+    <Badge variant="secondary">{tag}</Badge>{` `}
+  </span>
+), tags || [])
 
 const CardFace = ({ side, brand }) => (
   <div style={{ "margin-bottom": "1em" }}>
     <Card>
       <Card.Body>
-        <strong><u>{side.name} ({brand})</u></strong>
-        <Badge variant="secondary">{join(', ', side.tags || [])}</Badge>
+        <strong><u>{side.name} ({brand})</u></strong>{` `}
+        {multiBadge(side.tags)}
         <p>{side.desc}</p>
         <ul>
           {(side.prompts || []).map(prompt => (
