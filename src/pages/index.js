0a6356add65e8fdfdc9f2cf0eb173e3ea38a6eca
Bill Garrett <garrett@peppermile.com>
2020-01-03 02:11:31 -0800

    Organize cards by nested tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,18 +10,61 @@ import slug from 'slug'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import { append, groupBy, keys, map, pluck, pathOr, reduce } from 'ramda'
+import { assoc, groupBy, map, mergeDeepRight, nth, partition, pipe, pluck, pathOr } from 'ramda'
 
-const groupedCards = data => {
-  const cardGroups = groupBy(edge => pathOr(['no tags'], ['context', 'card', 'front', 'tags'], edge)[0], pluck('node', data.allSitePage.edges))
-  return reduce(
-    (a, tag) => append({
-      tag,
-      nodes: cardGroups[tag]
-    }, a),
-    [],
-    keys(cardGroups)
-  )
+// Return a Link element pointing to a given tag
+const linkToTag = tag => <Link style={{"color": "inherit", "text-decoration": "inherit"}} to={`/tags/${slug(tag).toLowerCase()}`}>{tag}</Link>
+
+// Return a Link element pointing to an individual card, loaded from GraphQL
+const linkToCard = card => <Link to={`/tags/${slug(card.name).toLowerCase()}`}>{card.name}</Link>
+
+const groupCards = (cards, depth, side) => groupBy(card => nth(depth, pathOr([], [side, 'tags'], card)), cards)
+
+const cardHasMoreTags = (card, nextDepth) => (pathOr(0, ['front', 'tags', 'length'], card) > nextDepth || pathOr(0, ['front', 'tags', 'length'], card) > nextDepth)
+
+const groupCardsByTagDepth = (cards, depth) => {
+  const depthCards = mergeDeepRight(groupCards(cards, depth, 'front'), groupCards(cards, depth, 'back'))
+  const formattedDepthCards = map(cardList => {
+    const nextDepth = depth + 1
+    const [childCards, newCardList] = partition(card => cardHasMoreTags(card, nextDepth), cardList)
+    let newContent = {
+      depth,
+      cardList: newCardList
+    }
+    if (childCards) {
+      newContent = assoc('children', groupCardsByTagDepth(childCards, nextDepth), newContent)
+    }
+    return newContent
+  }, depthCards)
+  return formattedDepthCards
+}
+
+const sequenceCardTree = (formattedDepthCards, currentSequence) => {
+  const sortedKeys = [...Object.keys(formattedDepthCards)].sort()
+  for (let key of sortedKeys) {
+    const data = formattedDepthCards[key]
+    const Header = `h${(data.depth+2)}`
+    currentSequence.push(<Header>{linkToTag(key)}</Header>)
+    currentSequence.push((
+      <ul>
+        {map(card => <li>{linkToCard(card)}</li>, data.cardList)}
+      </ul>
+    ))
+    if (data.children) {
+      currentSequence = sequenceCardTree(data.children, currentSequence)
+    }
+  }
+  return currentSequence
+}
+
+const groupCardsByTag = data => {
+  const cards = pipe(
+    pluck('node'),
+    pluck('context'),
+    pluck('card')
+  )(data.allSitePage.edges)
+  const formattedDepthCards = groupCardsByTagDepth(cards, 0)
+  return sequenceCardTree(formattedDepthCards, [])
 }
 
 const IndexPage = ({ data }) => (
@@ -30,22 +73,9 @@ const IndexPage = ({ data }) => (
     <Container>
       <Row>
         <Col sm={6}>
-          {map((cardGroup) => (
-            <div>
-              <h3>
-                <Link style={{"color": "inherit", "text-decoration": "inherit"}} to={`/tags/${slug(cardGroup.tag).toLowerCase()}`}>{cardGroup.tag}</Link>
-              </h3>
-              <ul>
-                {map(node => (
-                  <li>
-                    <Link to={node.path}>{node.context.card.name}</Link>
-                  </li>
-                ), cardGroup.nodes)}
-              </ul>
-              <pre>{JSON.stringify(cardGroup.cards)}</pre>
-            </div>
-          ), groupedCards(data))}
-
+          <pre>
+            {groupCardsByTag(data)}
+          </pre>
         </Col>
         <Col sm={6}>
           <h3>Articles and Background</h3>
