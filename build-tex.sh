a98b00e9050673b25a07a99201c1a6b52f51286f
Bill Garrett <garrett@peppermile.com>
2020-01-26 15:28:52 -0800

    Improve build-tex script to perform all steps

--- build-tex.sh
+++ build-tex.sh
@@ -2,5 +2,8 @@
 
 set -xueo pipefail
 
+node create-tex-includes.js
 xelatex -synctex=1 -interaction=nonstopmode cards.tex 
-cp cards.pdf static/
+sh ./cleanup-cards.sh
+
+rm allcards.tex cards.{aux,log,synctex.gz}
