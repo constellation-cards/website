6dcc2b81742c80f469fe77e2b77b2a5733520245
Bill Garrett <garrett@astralfrontier.org>
2020-05-28 00:50:36 -0700

    Spruce up the main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,55 +1,103 @@
 import React from "react"
-import { graphql, Link } from 'gatsby'
-import slug from 'slug'
-import Grid from '@material-ui/core/Grid';
-
+import { graphql, Link } from "gatsby"
+import slug from "slug"
+import Grid from "@material-ui/core/Grid"
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import { assoc, groupBy, map, mergeDeepRight, nth, partition, pathOr } from 'ramda'
+import {
+  assoc,
+  groupBy,
+  join,
+  last,
+  map,
+  mergeDeepRight,
+  nth,
+  partition,
+  pathOr,
+} from "ramda"
 
 const upper = s => s.replace(/^\w/, c => c.toUpperCase())
 
+const tagLinkStyle = {
+  color: "inherit",
+  textDecoration: "inherit",
+}
+
 // Return a Link element pointing to a given tag
-const linkToTag = tag => <Link style={{"color": "inherit", "text-decoration": "inherit"}} to={`/tags/${slug(tag).toLowerCase()}`}>{upper(tag)}</Link>
+const linkToTag = keypath => (
+  <Link style={tagLinkStyle} to={`/tags/${slug(last(keypath)).toLowerCase()}`}>
+    {join(" / ", map(upper, keypath))}
+  </Link>
+)
 
-// Return a Link element pointing to an individual card, loaded from GraphQL
-const linkToCard = card => <Link to={`/cards/${slug(card.name).toLowerCase()}`}>{card.name}</Link>
+const groupCards = (cards, depth, side) =>
+  groupBy(card => nth(depth, pathOr([], [side, "tags"], card)), cards)
 
-const groupCards = (cards, depth, side) => groupBy(card => nth(depth, pathOr([], [side, 'tags'], card)), cards)
-
-const cardHasMoreTags = (card, nextDepth) => (pathOr(0, ['front', 'tags', 'length'], card) > nextDepth || pathOr(0, ['front', 'tags', 'length'], card) > nextDepth)
+const cardHasMoreTags = (card, nextDepth) =>
+  pathOr(0, ["front", "tags", "length"], card) > nextDepth ||
+  pathOr(0, ["front", "tags", "length"], card) > nextDepth
 
 const groupCardsByTagDepth = (cards, depth) => {
-  const depthCards = mergeDeepRight(groupCards(cards, depth, 'front'), groupCards(cards, depth, 'back'))
+  const depthCards = mergeDeepRight(
+    groupCards(cards, depth, "front"),
+    groupCards(cards, depth, "back")
+  )
   const formattedDepthCards = map(cardList => {
     const nextDepth = depth + 1
-    const [childCards, newCardList] = partition(card => cardHasMoreTags(card, nextDepth), cardList)
+    const [childCards, newCardList] = partition(
+      card => cardHasMoreTags(card, nextDepth),
+      cardList
+    )
     let newContent = {
       depth,
-      cardList: newCardList
+      cardList: newCardList,
     }
     if (childCards) {
-      newContent = assoc('children', groupCardsByTagDepth(childCards, nextDepth), newContent)
+      newContent = assoc(
+        "children",
+        groupCardsByTagDepth(childCards, nextDepth),
+        newContent
+      )
     }
     return newContent
   }, depthCards)
   return formattedDepthCards
 }
 
-const sequenceCardTree = (formattedDepthCards, currentSequence) => {
+const sequenceCardTree = (
+  formattedDepthCards,
+  currentSequence,
+  currentKeys = []
+) => {
   const sortedKeys = [...Object.keys(formattedDepthCards)].sort()
   for (let key of sortedKeys) {
+    let keypath = [...currentKeys, key]
     const data = formattedDepthCards[key]
-    const Header = `h${(data.depth+1) * 2}`
-    currentSequence.push(<Header>{linkToTag(key)}</Header>)
-    currentSequence.push((
-      <ul>
-        {map(card => <li>{linkToCard(card)}</li>, data.cardList)}
-      </ul>
-    ))
+    if (data.cardList && data.cardList.length > 0) {
+      currentSequence.push()
+      currentSequence.push(
+        <Grid item md={3}>
+          <h3>{linkToTag(keypath)}</h3>
+          <List dense>
+          {map(card => (
+            <ListItem button component={Link} to={`/cards/${slug(card.name).toLowerCase()}`}>
+              <ListItemText primary={card.name} />
+            </ListItem>
+          ), data.cardList)}
+          </List>
+        </Grid>
+      )
+    }
     if (data.children) {
-      currentSequence = sequenceCardTree(data.children, currentSequence)
+      currentSequence = sequenceCardTree(
+        data.children,
+        currentSequence,
+        keypath
+      )
     }
   }
   return currentSequence
@@ -64,17 +112,17 @@ const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <Grid container spacing={3}>
+      {groupCardsByTag(data)}
       <Grid item md={6}>
-        {groupCardsByTag(data)}
-      </Grid>
-      <Grid md={6}>
         <h3>Articles and Background</h3>
         <ul>
-          {map(node => (
-            <li>
-              <Link to={node.path}>{node.context.frontmatter.title}</Link>
-            </li>
-          ), data.allSitePage.nodes
+          {map(
+            node => (
+              <li>
+                <Link to={node.path}>{node.context.frontmatter.title}</Link>
+              </li>
+            ),
+            data.allSitePage.nodes
           )}
         </ul>
       </Grid>
@@ -85,27 +133,32 @@ const IndexPage = ({ data }) => (
 export default IndexPage
 
 export const query = graphql`
-query {
-  allCardFace {
-    nodes {
-      name
-      front {
-        tags
-      }
-      back {
-        tags
+  query {
+    allCardFace {
+      nodes {
+        name
+        front {
+          tags
+        }
+        back {
+          tags
+        }
       }
     }
-  }
-  allSitePage(filter: {context: {frontmatter: {draft: {ne: true}, title: {glob: "*"}}}}, sort: {fields: context___frontmatter___title, order: ASC}) {
-    nodes {
-      path
-      context {
-        frontmatter {
-          title
+    allSitePage(
+      filter: {
+        context: { frontmatter: { draft: { ne: true }, title: { glob: "*" } } }
+      }
+      sort: { fields: context___frontmatter___title, order: ASC }
+    ) {
+      nodes {
+        path
+        context {
+          frontmatter {
+            title
+          }
         }
       }
     }
   }
-}
 `
