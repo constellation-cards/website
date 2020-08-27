4092a68abe505ab3209d274f99e20cc9e23f0d77
Bill Garrett <garrett@astralfrontier.org>
2020-08-27 00:18:51 -0700

    Add gatsby-plugin-typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     author: `@astralfrontier`,
   },
   plugins: [
+    `gatsby-plugin-typescript`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
